9ad7c7341bbf2f6af613e8767bb8a8ade85f024c
gaurh00 <akshat.gaur_cs17@gla.ac.in>
2019-02-20 11:16:34 +0530

    Add

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,16 @@
 public class Node {
     private int data;
+    private Node next;
+        public int getdata(){
+            return data;
+
+        }
+        public void setdata(int data){
+            this.data=data;
+
+        }
 
 }
+
 
 
